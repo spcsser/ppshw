b3f755a935cd032fe7f85b3c6fee82c7885ad570
Swen <swen.jung@psx-technology.com>
2013-02-01 23:28:31 +0100

    Changed pdf link to host dependent link

--- public/javascripts/filetree.js
+++ public/javascripts/filetree.js
@@ -1,22 +1,32 @@
 FileTree={
-    selectDocument:function(id){
-      $('div#frontpage iframe').attr('src','/pdf.js/viewer.html?file=http://localhost:3000/file/get/pdf/'+id);
-    },
-    showTagMenu:function(elem){
-      console.log(elem);
-    },
-    initLinks:function(){
-      $('.ft_l2_entry')
-        .live('click',function(e){
-          FileTree.selectDocument($(this).attr('id'));
-        })
-        .find('.tagBtn')
-        .live('click', function(e){
-          e.stopPropagation();
-          FileTree.showTagMenu($(this));
-        })
-      ;
-    },
+  baseUrl:null,
+  getBaseUrl:function(){
+    if(this.baseUrl===null){
+        var bUrl=window.location.href;
+        console.log(bUrl);
+        this.baseUrl=bUrl;
+    }
+    return this.baseUrl;
+  },
+  selectDocument:function(id){
+      $('div#frontpage iframe').attr('src','/pdf.js/viewer.html?file='+this.getBaseUrl()+'file/get/pdf/'+id);
+  },
+  showTagMenu:function(elem){
+    console.log(elem);
+  },
+  initLinks:function(){
+    this.getBaseUrl();
+    $('.ft_l2_entry')
+      .live('click',function(e){
+        FileTree.selectDocument($(this).attr('id'));
+      })
+      .find('.tagBtn')
+      .live('click', function(e){
+        e.stopPropagation();
+        FileTree.showTagMenu($(this));
+      })
+    ;
+  },
 };
 
 $(document).ready(function(event){
